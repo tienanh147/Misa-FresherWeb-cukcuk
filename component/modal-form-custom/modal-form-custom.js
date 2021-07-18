d2a2cb17521e95de58003a5be22bef1a5a7dabd8
tienanh147 <list>
2021-07-18 22:24:38 +0700

    add modal-form

---
+++ component/modal-form-custom/modal-form-custom.js
@@ -0,0 +1,8 @@
+window.onclick = function(event) {
+    var modal = document.querySelector('.modal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    } else {
+
+    }
+}
