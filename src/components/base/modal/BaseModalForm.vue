aef3918d32869f4e2b7bc5e1c6599276073cb432
tienanh147 <list>
2021-08-12 13:16:40 +0700

    add dialog and validate data

--- src/components/base/modal/BaseModalForm.vue
+++ src/components/base/modal/BaseModalForm.vue
@@ -3,7 +3,7 @@
     <div class="form-wrapper animate">
       <div class="form-wrapper-header">
         THÔNG TIN NHÂN VIÊN
-        <div class="icon-default icon-x x-btn" @click="hideModalForm"></div>
+        <div class="icon-default icon-x x-btn" @click="btnCancelClick"></div>
       </div>
 
       <div class="form-content">
@@ -32,12 +32,14 @@
 
           <BaseInput
             placeholder=""
+            ref="FullName"
             label="Họ và tên(*)"
             :required="true"
             warningLabel="Thông tin này là bắt buộc"
             v-model="formData.FullName"
           />
           <BaseInput
+            ref="DateOfBirth"
             type="date"
             label="Ngày sinh"
             v-model="formData.DateOfBirth"
@@ -45,23 +47,27 @@
 
           <BaseInput label="Giới tính" v-model="formData.Gender" />
           <BaseInput
+            ref="IdentityNumber"
             label="Số CMTND/ Căn cước(*)"
             :required="true"
             v-model="formData.IdentityNumber"
           />
 
           <BaseInput
+            ref="IdentityDate"
             type="date"
             label="Ngày cấp"
             v-model="formData.IdentityDate"
           />
 
           <BaseInput
+            ref="IdentityPlace"
             :style="{ 'flex-basis': '100%' }"
             label="Nơi cấp"
             v-model="formData.IdentityPlace"
           />
           <BaseInput
+            ref="Email"
             placeholder="example@domain.com"
             label="Email(*)"
             validate="email"
@@ -69,6 +75,7 @@
             v-model="formData.Email"
           />
           <BaseInput
+            ref="PhoneNumber"
             label="Số điện thoại(*)"
             type="number"
             :required="true"
@@ -79,9 +86,10 @@
             B. THÔNG TIN CÔNG VIỆC
             <div class="color-line"></div>
           </div>
-          <div class="input-wrapper" v-if="gotSelectBoxes">
+          <div class="input-wrapper" v-if="gotDetail">
             <label for="Position">Vị trí</label>
             <SelectBox
+              ref="PositionId"
               style="height: 35px"
               :selectBoxData="selectBoxesData[0]"
               :idField="selectBoxesData[0][1]"
@@ -90,9 +98,10 @@
               v-model="formData.PositionId"
             />
           </div>
-          <div class="input-wrapper" v-if="gotSelectBoxes">
+          <div class="input-wrapper" v-if="gotDetail">
             <label for="Department">Phòng ban</label>
             <SelectBox
+              ref="DepartmentId"
               style="height: 35px"
               :selectBoxData="selectBoxesData[1]"
               :idField="selectBoxesData[1][1]"
@@ -102,28 +111,22 @@
             />
           </div>
           <BaseInput
+            ref="PersonalTaxCode"
             label="Mã số thuế cá nhân"
             v-model="formData.PersonalTaxCode"
           />
-          <div class="input-wrapper" style="position: relative">
-            <label for="Salary">Mức lương cơ bản</label>
-            <input
-              type="number"
-              class="text-box-default input-item"
-              id="Salary"
-              v-model="formData.Salary"
-            />
-            <div class="money-unit">(VND)</div>
-          </div>
+          <CurrencyInput v-model="formData.Salary" />
           <BaseInput
+            ref="JoinDate"
             type="date"
             label="Ngày gia nhập"
             v-model="formData.JoinDate"
           />
 
-          <div class="input-wrapper" v-if="gotSelectBoxes">
+          <div class="input-wrapper" v-if="gotDetail">
             <label for="WorkStatus">Tình trạng công việc</label>
             <SelectBox
+              ref="WorkStatus"
               style="height: 35px"
               :dropup="true"
               :selectBoxData="selectBoxesData[2]"
@@ -140,7 +143,7 @@
         <button
           class="button button-icon"
           id="buttonCancel"
-          @click="hideModalForm"
+          @click="btnCancelClick"
         >
           Hủy
         </button>
@@ -148,22 +151,32 @@
           objectId
           class="button button-icon"
           id="buttonSave"
-          @click="saveModalForm"
+          @click="btnSaveClick"
         >
           <i class="far fa-save"></i>
           Lưu
         </button>
       </div>
     </div>
+    <Dialog
+      v-if="dialog.isShow"
+      :header="dialog.header"
+      :content="dialog.content"
+      :type="dialog.type"
+      :confirmBtn="dialog.confirmBtn"
+      :cancelBtn="dialog.cancelBtn"
+    />
   </div>
 </template>
 
 <script>
+import Dialog from "../dialog/BaseDialog.vue";
 import SelectBox from "../select-custom/BaseSelectBox.vue";
 import BaseInput from "../input/BaseInput.vue";
+import CurrencyInput from "../input/CurrencyInput.vue";
 export default {
   name: "ModalForm",
-  components: { SelectBox, BaseInput },
+  components: { SelectBox, BaseInput, CurrencyInput, Dialog },
   props: {
     isShow: {
       type: Boolean,
@@ -182,35 +195,143 @@ export default {
     }
   },
 
-  created() {
-  },
-  mounted() {
-  
-  },
+  created() {},
+  mounted() {},
   data() {
     return {
       gotSelectBoxes: true,
       // selectBoxesData: [],
       formData: {},
-      testInput: "outsideData",
-      gotDetail: false
+      gotDetail: false,
+
+      /**
+       * các thông số của dialog popup
+       */
+      dialog: {
+        isShow: false,
+        header: "Xóa bản ghi",
+        content: "Bạn có chắc muốn xóa các nhân viên hay không?",
+        type: "warning-red",
+        confirmBtn: { content: "Xóa", function: null },
+        cancelBtn: { content: "Tiếp tục nhập", function: null }
+      },
+
+      focused: document.activeElement
     };
   },
   methods: {
+    btnCancelClick() {
+      var vm = this;
+      var code = this.formData.EmployeeCode;
+      var dialogSetting = {
+        type: "warning-yellow",
+        header:
+          vm.formMode == 2
+            ? "Đóng form thông tin chi tiết"
+            : "Đóng form thêm mới nhân viên",
+        content:
+          vm.formMode == 2
+            ? `Bạn có muốn đóng form thông tin của nhân viên "<b>${code}</b>" không?`
+            : `Bạn có muốn đóng form thêm mới nhân viên "<b>${code}</b>" không?`,
+        cancelBtn: {
+          content: "Tiếp tục nhập",
+          function: function() {
+            vm.dialog.isShow = false;
+          }
+        },
+        confirmBtn: {
+          content: "Đóng",
+          function: vm.hideModalForm
+        },
+        isShow: true
+      };
+      this.dialog = dialogSetting;
+    },
     hideModalForm() {
+      this.dialog.isShow = false;
       this.$emit("hideForm");
     },
-    getDetail() {
-      this.axios
+    validateOnSave() {
+      var refs = this.$refs;
+      for (var prop in refs) {
+        var ref = refs[prop];
+        if (ref.required) {
+          if (ref.inputValue == null || ref.inputValue == "") {
+            ref.$el.querySelector("input").focus();
+            ref.warning = "thông tin này là bắt buộc";
+            return false;
+          }
+        }
+        if (ref.validate != null && ref.warning != null) {
+          ref.$el.querySelector("input").focus();
+          return false;
+        }
+      }
+      // this.focused = document.activeElement;
+      return true;
+    },
+    btnSaveClick() {
+      if (this.validateOnSave()) {
+        this.saveModalForm();
+      }
+    },
+    saveModalForm() {
+      this.formData["Gender"] = parseInt(this.formData["Gender"]);
+      this.formData["Salary"] = parseInt(this.formData["Salary"]);
+      var dataPost = JSON.stringify(this.formData);
+      if (this.formMode == 0) {
+        this.axios
+          .post("http://cukcuk.manhnv.net/v1/Employees", dataPost, {
+            headers: {
+              // Overwrite Axios's automatically set Content-Type
+              "Content-Type": "application/json"
+            }
+          })
+          .then(res => {
+            if (res) {
+              console.log("Lưu thành công");
+              this.$emit("refreshData");
+              this.hideModalForm();
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      } else if (this.formMode == 2) {
+        this.axios
+          .put(
+            `http://cukcuk.manhnv.net/v1/Employees/${this.formDataId}`,
+            dataPost,
+            {
+              headers: {
+                // Overwrite Axios's automatically set Content-Type
+                "Content-Type": "application/json"
+              }
+            }
+          )
+          .then(res => {
+            if (res) {
+              console.log("Lưu thành công");
+              this.$emit("refreshData");
+              this.$refs['EmployeeCode'].$el.querySelector('input').focus();
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    },
+
+    getDetail: async function() {
+      await this.axios
         .get(`http://cukcuk.manhnv.net/v1/Employees/${this.formDataId}`)
         .then(res => {
-          this.formData = res.data;
-          console.log(this.formData);
-          this.gotDetail = true;
+          if (res.status == 200) this.formData = res.data;
         })
-        .catch(res => {
-          console.log(res);
+        .catch(error => {
+          console.log(error.response.status);
         });
+      this.gotDetail = true;
     },
 
     getSelectBoxesData() {
@@ -241,79 +362,52 @@ export default {
         });
       });
     },
+
     getNewCode: async function(codeField) {
       await this.axios
         .get("http://cukcuk.manhnv.net/v1/Employees/NewEmployeeCode")
         .then(res => {
-          // console.log(res.data);
-          this.$set(this.formData, codeField, res.data);
-          // this.formData[codeField] = res.data;
-          this.gotDetail = true;
+          if (res.status == 200) this.$set(this.formData, codeField, res.data);
         })
         .catch(error => {
-          console.log(error);
+          console.log(error.response.status);
         });
-    },
-    saveModalForm() {
-      this.formData["Gender"] = parseInt(this.formData["Gender"]);
-      this.formData["Salary"] = parseInt(this.formData["Salary"]);
-      var dataPost = JSON.stringify(this.formData);
-      // console.log(JSON.stringify(this.formData));
-      var request;
-      if (this.formMode == 0) {
-        request = this.axios.post(
-          "http://cukcuk.manhnv.net/v1/Employees",
-          dataPost,
-          {
-            headers: {
-              // Overwrite Axios's automatically set Content-Type
-              "Content-Type": "application/json"
-            }
-          }
-        );
-      } else if (this.formMode == 2) {
-        request = this.axios.put(
-          `http://cukcuk.manhnv.net/v1/Employees/${this.formDataId}`,
-          dataPost,
-          {
-            headers: {
-              // Overwrite Axios's automatically set Content-Type
-              "Content-Type": "application/json"
-            }
-          }
-        );
-      }
-
-      request
-        .then(res => {
-          if (res) {
-            console.log("Lưu thành công");
-          }
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      this.gotDetail = true;
     }
   },
-  computed: {},
+  computed: {
+    activeInput() {
+      var refs = this.$refs;
+      for (var prop in refs) {
+        var ref = refs[prop];
+        if (ref.$el.querySelector("input").hasFocus())
+          return ref.$el.querySelector("input");
+      }
+      return null;
+    }
+  },
   watch: {
     /**
      * nếu hiển thị form(isShow=true) thì lấy code mới hoặc lấy thông tin chi tiết
      * nếu ẩn form thì reset các trường
      */
-    isShow: function() {
-      if (this.isShow == true) {
+    isShow: async function(val) {
+      if (val) {
+        // this.gotSelectBoxes = true;
         if (this.formMode == 0) {
-          this.getNewCode("EmployeeCode");
+          await this.getNewCode("EmployeeCode");
         } else if (this.formMode == 2) {
-          this.getDetail();
+          await this.getDetail();
         }
+        this.$nextTick(function() {
+          this.$refs.EmployeeCode.$el.querySelector("input").focus();
+        });
       } else {
-        // this.gotSelectBoxes=false;
+        // this.gotSelectBoxes = false;
         this.gotDetail = false;
         this.formData = {};
       }
-    }
+    },
   }
 };
 </script>
