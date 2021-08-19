55754196ac8751003fb8d6f6d166994c8d8d78fc
tienanh147 <list>
2021-08-19 10:37:50 +0700

    enum type

---
+++ MISA.Core/Enum/MISAEnum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.Core.Enum
+{
+    /// <summary>
+    /// Giới tính
+    /// </summary>
+    /// CreatedBy: TTAnh(17/08/2021)
+    public enum Gender
+    {
+        Male=1,
+        Female=0,
+        Other=2
+    }
+
+    /// <summary>
+    /// Trạng thái công việc
+    /// </summary>
+    public enum WorkStatus
+    {
+        /// <summary>
+        /// Đang làm việc
+        /// </summary>
+        Working=1,
+        /// <summary>
+        /// Thôi việc
+        /// </summary>
+        QuitJob=2,
+        /// <summary>
+        /// Nghỉ phép
+        /// </summary>
+        OffJob=3,
+        /// <summary>
+        /// Đuổi việc
+        /// </summary>
+        Fire=4,
+
+        None=0
+   
+    }
+
+    public enum ValidateType
+    {
+        Unique = 1,
+
+    }
+}
