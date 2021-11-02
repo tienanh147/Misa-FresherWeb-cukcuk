c5e7d3d8eba22139f007afcded66f1176343122a
tienanh147 <tienanh1472000@gmail.com>
2021-11-02 14:22:11 +0700

    config launch and resource

--- MISA.CukCuk/Properties/Resources.Designer.cs
+++ MISA.CukCuk/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MISA.CukCuk.Properties {
+namespace MISA.CoreWebApp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MISA.CukCuk.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MISA.CukCuk.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MISA.CoreWebApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
